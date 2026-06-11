1ffbc1fb9864a5d61f5c7571efdfc472b331a387
Soheyl Ashena <sashena203@gmail.com>
2026-06-11 16:55:03 +0330

    a

--- src/handlers/message/admin-reply.ts
+++ src/handlers/message/admin-reply.ts
@@ -1,12 +1,12 @@
 import { adminReplyMode, bot, waitingForBalance } from "../../config";
 
 export async function adminReplyHandler(message: string, senderID: number) {
+  const targetUserId = adminReplyMode.get(senderID);
+  if (!targetUserId) return;
+
   if (waitingForBalance.get(senderID)) {
     waitingForBalance.delete(senderID);
   }
-
-  const targetUserId = adminReplyMode.get(senderID);
-  if (!targetUserId) return;
 
   await bot.sendMessage(
     targetUserId,
