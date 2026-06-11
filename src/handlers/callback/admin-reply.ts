fe870aeee88165e7a1f8f2a6c32d1ba06e3e3735
Soheyl Ashena <sashena203@gmail.com>
2026-06-11 16:13:09 +0330

    a

--- src/handlers/callback/admin-reply.ts
+++ src/handlers/callback/admin-reply.ts
@@ -1,22 +1,21 @@
 import { adminIDs, adminReplyMode, bot, waitingForBalance } from "../../config";
 
 export async function adminReplyHandler(data: string, senderID: number) {
-  if (!data.startsWith("reply_") || !data.startsWith("block_") || !data.startsWith("approve_")) return;
+  if (!data.startsWith("reply_") && !data.startsWith("block_") && !data.startsWith("approve_")) return;
 
   if (!adminIDs.includes(senderID)) return;
 
   const dataArr = data.split("_");
   const mode = dataArr[0];
-  const recieverID = Number(dataArr[1]);
-
+  const receiverID = Number(dataArr[1]);
   try {
     switch (mode) {
-      case "relpy":
-        adminReplyMode.set(senderID, recieverID);
+      case "reply":
+        adminReplyMode.set(senderID, receiverID);
         await bot.sendMessage(senderID, "✍️ پیام خود را برای مشتری ارسال کنید.");
         break;
       case "approve":
-        waitingForBalance.set(senderID, recieverID);
+        waitingForBalance.set(senderID, receiverID);
         await bot.sendMessage(senderID, "مقدار موجودی برای افزایش را وارد کنید:");
         break;
       case "block":
