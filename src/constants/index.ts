d23d6797dcc11b8a2f048bd5531f53d1d8e6c51d
Soheyl Ashena <115019489+SoheylAshena@users.noreply.github.com>
2026-06-11 21:36:31 +0330

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,56 +1,14 @@
 export const PRODUCTS = [
   {
-    text: "10 گیگ",
-    amount: 50000,
+    text: "10 گیگ یک ماهه | 20,000 تومان",
+    amount: 20000,
     callback_data: "vpn_1",
     style: "primary",
   },
   {
-    text: "30 گیگ",
-    amount: 120000,
+    text: "نامحدود یک ماهه | 99,000 تومان",
+    amount: 99000,
     callback_data: "vpn_2",
-    style: "primary",
-  },
-  {
-    text: "50 گیگ",
-    amount: 175000,
-    callback_data: "vpn_3",
-    style: "primary",
-  },
-  {
-    text: "100 گیگ",
-    amount: 250000,
-    callback_data: "vpn_4",
-    style: "primary",
-  },
-  {
-    text: "نامحدود",
-    amount: 350000,
-    callback_data: "vpn_5",
-    style: "primary",
-  },
-  {
-    text: "سرویس گیمینگ: 1 گیگ",
-    amount: 250000,
-    callback_data: "vpn_6",
-    style: "primary",
-  },
-  {
-    text: "وایرگارد نامحدود دوکاربره",
-    amount: 690000,
-    callback_data: "wire_1",
-    style: "primary",
-  },
-  {
-    text: "ویندسکرایب نامحدود تک کاربره",
-    amount: 325000,
-    callback_data: "wind_1",
-    style: "primary",
-  },
-  {
-    text: "هوش مصنوعی gemini",
-    amount: 590000,
-    callback_data: "ai_1",
     style: "primary",
   },
 ] as const;
